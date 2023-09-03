44ac272ec43cb58fe21352f901550fa86b328da2
Rennerx3 <rmokosce21@hotmail.com>
2023-09-03 20:15:38 -0300

    index

---
+++ logica/index.js
@@ -0,0 +1,76 @@
+const d = document;
+
+d.addEventListener("DOMContentLoaded",slide);
+
+function slide(){
+   let $slide = d.querySelectorAll(".slider-slide"),
+       $prevBtn = d.querySelector(".prev"),
+       $nextBtn = d.querySelector(".next"),
+       $start = d.querySelector(".start"),
+       $middle = d.querySelector(".middle"),
+       $final = d.querySelector(".final"),
+       $sliderP = d.querySelectorAll(".circle-button");
+
+       let i = 0;   
+
+    d.addEventListener("click", e=>{
+        if(e.target === $prevBtn){
+            e.preventDefault();
+            $slide[i].classList.remove("active");
+            $sliderP[i].classList.remove("active");
+            i--;
+
+            if( i < 0){
+                i = $slide.length - 1;
+            }
+
+            $slide[i].classList.add("active");
+            $sliderP[i].classList.add("active");
+        }
+        if(e.target === $nextBtn){
+            e.preventDefault();
+            $slide[i].classList.remove("active");
+            $sliderP[i].classList.remove("active");
+            i++;
+            
+            if(i >= $slide.length){
+                i = 0;
+            }
+            $slide[i].classList.add("active");
+            $sliderP[i].classList.add("active");
+        }
+
+        if(e.target === $start){
+            e.preventDefault();
+            $slide[0].classList.add("active");
+            $slide[1].classList.remove("active");
+            $slide[2].classList.remove("active");
+            $start.classList.add("active");
+            $middle.classList.remove("active");
+            $final.classList.remove("active");
+            i = 0;
+
+        }
+        if(e.target === $middle){
+         e.preventDefault();
+         $slide[1].classList.add("active");  
+         $slide[0].classList.remove("active");
+         $slide[2].classList.remove("active"); 
+         $middle.classList.add("active");
+         $start.classList.remove("active");
+         $final.classList.remove("active");
+         i = 1;
+        }
+        
+        if(e.target === $final){
+            e.preventDefault();
+            $slide[2].classList.add("active");
+            $slide[1].classList.remove("active");
+            $slide[0].classList.remove("active"); 
+            $final.classList.add("active");
+            $middle.classList.remove("active");
+            $start.classList.remove("active"); 
+            i = 2;
+        }
+    })
+}
