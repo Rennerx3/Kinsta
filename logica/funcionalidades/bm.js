1c4363f86d9d1432af970bae9383b0a28c5da7f1
Rennerx3 <rmokosce21@hotmail.com>
2023-09-06 13:12:38 -0300

    movile

--- logica/funcionalidades/bm.js
+++ logica/funcionalidades/bm.js
@@ -4,11 +4,13 @@ export default function burgerMenu(){
 
     let $btn = d.querySelector(".btn"),
         $panel = d.querySelector(".panel"),
-        $menu = d.querySelector(".menu");
+        $menu = d.querySelector(".menu"),
+        $search = d.querySelector(".head-input");
     
     d.addEventListener("click", e =>{
         if(e.target.matches(".btn") || e.target.matches(".btn *")){
             $panel.classList.toggle("is-active");
+            $search.classList.toggle("is-active");
         }
         
         if(e.target.matches(".menu *")){
